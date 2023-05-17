3e6348063772ec0339c207a35a244eee74c24ba9
Dthesle <80913781+Dthesle@users.noreply.github.com>
2023-05-17 17:25:32 -0500

    Update RCOInstaller.sh
    Signed-off-by: Dthesle <80913781+Dthesle@users.noreply.github.com>

--- RCOInstaller.sh
+++ RCOInstaller.sh
@@ -5,7 +5,7 @@ echo "#!/bin/sh" >> RobloxPlayer
 echo "rm -rf /Applications/Roblox.app/Contents/MacOS/ClientSettings && mkdir /Applications/Roblox.app/Contents/MacOS/ClientSettings && curl https://raw.githubusercontent.com/Dthesle/RCO-Code/main/ClientAppSettings.json -o /Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json && exec /Applications/Roblox.app/Contents/MacOS/ActualRobloxPlayer" >> RobloxPlayer
 chmod +x RobloxPlayer
 touch RCOPATCHED
-echo "Finished installing RCO"
+echo "Unlocked!"
 else
-echo "You have already installed RCO!"
+echo "Are you sped? It's installed already."
 fi
